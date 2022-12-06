2f3a1722a020d01d78642f1e9500c05b89432875
Jamie Johnstone <j@miej.co>
2022-12-06 16:09:48 +0000

    Prune domain list

--- domains/vars.tf
+++ domains/vars.tf
@@ -5,11 +5,7 @@ variable "domains" {
     description = "List of domains to generate records for"
     default = [
       "jamiejohnstone.com",
-      "jamesjohnstone.co",
       "mesj.co",
       "miej.co",
-      "miej.co.nz",
-      "miej.co.uk",
-      "jamiejohnstone.co.uk"
     ]
 }
